a5c4c0600385c026f9a9fe182917c8309a3e14cf
MpilaA <mpila@outlook.es>
2025-10-10 19:24:59 +0200

    De la actividad 4 de la unidad 2 tengo hecho el ejercicio 1 y 2 sin compilar y comenzado el ejercicio 3.

--- UD2_Actividad_4/src/main.java
+++ UD2_Actividad_4/src/main.java
@@ -5,10 +5,36 @@ public class main {
 
         // Ejercicio 1
         Scanner scan = new Scanner(System.in);
-        int [] nums1 = new int[10];
+        double [] nums1 = new double[10];
 
         for (int i = 0; i < nums1.length; i++) {
-            System.out.println("Dame un numero: ");
+            System.out.print("Dame un numero: ");
+            nums1[i] = scan.nextDouble();
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.println(nums1[i]);
+        }
+        // Ejercicio 2 
+        scan = new Scanner(System.in);
+        double [] nums2 = new double[10];
+        
+        for (int i = 0; i < nums2.length; i++) {
+            System.out.print("Dame un numero: ");
+            nums2[i] = scan.nextDouble();
+        }
+
+        double suma2 = 0;
+        for (int i = 0; i < nums2.length; i++) {
+            suma2 += nums2[i];
+        }
+        System.out.println("La suma de todos los numeros introducidos es " + suma2);
+
+        // Ejercicio 3
+        scan = new Scanner(System.in);
+        double [] nums3 = new double[10];
+
+        for (int i = 0; i < nums3.length; i++) {
+            
         }
 
     }
