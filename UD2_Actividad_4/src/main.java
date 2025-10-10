79deb881f6f37b3304332d7d1015dea7132000f7
COMPU-S028\daw1 <example>
2025-10-10 11:39:37 +0200

    Comienzo de la Actividad 4 ejercicio1.

---
+++ UD2_Actividad_4/src/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+
+        // Ejercicio 1
+        Scanner scan = new Scanner(System.in);
+        int [] nums1 = new int[10];
+
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.println("Dame un numero: ");
+        }
+
+    }
+}
