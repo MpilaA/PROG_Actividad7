d913151f67160c9e378bbacc3eb0893bf064a5a2
COMPU-S028\daw1 <example>
2025-10-22 09:51:39 +0200

    Ejercicio 1 hecho y ejercicio 2 empezado de la actividad 6 de la unidad 2.

---
+++ UD2_Actividad_06/src/main.java
@@ -0,0 +1,35 @@
+public class main {
+    public static void main(String[] Args) {
+        // Ejercicio 1
+        // 5*5
+        int [][] matriz1 = new int[5][5];
+
+        for (int i = 0, numero = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1[i].length; j++) {
+                numero++;
+                matriz1[i][j] = numero;
+            }
+        }
+        for (int i = 0; i < matriz1.length; i++) {
+            System.out.print(i + ": " );
+            for (int j = 0; j < matriz1[i].length; j++) {
+                System.out.print(matriz1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        // Ejercicio 2
+        int [][] matriz2 = new int[10][10];
+
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[i].length; j++) {
+                matriz2[i][j] = (i + 1)*(j + 1);
+            }
+        }
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[i].length; j++) {
+                S
+            }
+        }
+
+    }
+}
